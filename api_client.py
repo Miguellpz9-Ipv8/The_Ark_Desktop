ba8e648ba831c82aab068c4fe9b63c17dafdedd3
Miguellpz9-Ipv8 <Miguellpz9@yahoo.com>
2024-01-23 14:46:07 -0800

    Update api_client.py
      -Replaced conventional string concatenation with f-strings for string formatting.
      -Combined the generation of p1_key and pk1_key into a single line for improved readability.
      -Used _ as a throwaway variable in the loop where it's not used. 
     -Renamed the variable message to encrypted_message in the send_message method for better clarity. 
      -Added a new method, get_encrypted_messages, to retrieve encrypted messages from the server. 
      -Introduced a disconnect method to close the session when needed.

--- api_client.py
+++ api_client.py
@@ -1,32 +1,46 @@
 import os
-
 from requests_tor import RequestsTor
-
 from security import DH_Endpoint
 import secrets
 import string
-alphabet = string.ascii_letters + string.digits
 
+alphabet = string.ascii_letters + string.digits
 class Client:
     def __init__(self, address):
         self.address = address
-        self.connect_url = "http://" + address + ".onion"
+        self.connect_url = f"http://{address}.onion"
         self.dh = DH_Endpoint()
         self.session = RequestsTor(tor_ports=(9050,), tor_cport=9051)
-        self.client_id = ''.join(secrets.choice(alphabet) for i in range(16))
+        self.client_id = ''.join(secrets.choice(alphabet) for _ in range(16))
 
     def connect(self):
-        p1_key = self.dh.generate_numbers()
-        pk1_key = self.dh.generate_numbers()
-        data = self.session.get(self.connect_url + "/handshake", json={"key": p1_key, "id": self.client_id}, params={'stage': "1"}).json()
-        #print(data)
+        p1_key, pk1_key = self.dh.generate_numbers(), self.dh.generate_numbers()
+        data = self.session.get(
+            f"{self.connect_url}/handshake",
+            json={"key": p1_key, "id": self.client_id},
+            params={'stage': "1"}
+        ).json()
+
         p2_key = int(data['key2'])
         self.dh = DH_Endpoint(p1_key, p2_key, pk1_key)
         key = self.dh.generate_full_key(data['pk2'])
-        #print(key)
-        self.session.get(self.connect_url + "/handshake", json={'pk1': self.dh.generate_partial_key(), "id": self.client_id}, params={'stage': "2"})
+
+        self.session.get(
+            f"{self.connect_url}/handshake",
+            json={'pk1': self.dh.generate_partial_key(), "id": self.client_id},
+            params={'stage': "2"}
+        )
 
     def send_message(self, text):
-        message = self.dh.encrypt_message(text).decode()
-        self.session.post(self.connect_url + "/get-message", json={'message': message, "id": self.client_id})
+        encrypted_message = self.dh.encrypt_message(text).decode()
+        self.session.post(
+            f"{self.connect_url}/get-message",
+            json={'message': encrypted_message, "id": self.client_id}
+        )
 
+    def get_encrypted_messages(self):
+        response = self.session.get(f"{self.connect_url}/get-encrypted-messages", params={'id': self.client_id})
+        return response.json().get('messages', [])
+
+    def disconnect(self):
+        self.session.close()
