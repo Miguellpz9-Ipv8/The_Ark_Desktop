49d5a17903db94c7a5a3c8aeb1016f3d7a5eaa6e
Miguellpz9-Ipv8 <Miguellpz9@yahoo.com>
2024-01-26 09:12:49 -0800

    Update api_server.py
     - Added start_tor function to improve structure and readability for maintainability
     - path concatenation
     - automated directory creation and check if it exists
     - Formatted code overall

--- api_server.py
+++ api_server.py
@@ -1,23 +1,26 @@
 import os
 import subprocess
-
-from stem.control import Controller
 import threading
 import socket
-from flask import Flask, request, session, jsonify
-from security import DH_Endpoint
-import random
-import secrets
+from flask import Flask, request, jsonify
+from security import DH_Endpoint, AESCipher
+from storage import Storage
 import string
-alphabet = string.ascii_letters + string.digits
+import secrets
+from stem.control import Controller
 
+alphabet = string.ascii_letters + string.digits
 datafolder = os.getenv("appdata") + "/the_ark/"
+database_path = os.path.join(datafolder, "data")
+messages_path = os.path.join(datafolder, "messages")
+
 try:
-    os.mkdir(datafolder)
+    os.makedirs(datafolder, exist_ok=True)
 except FileExistsError:
     pass
+
 app = Flask("The Ark")
-app.secret_key = ''.join(secrets.choice(alphabet) for i in range(256))
+app.secret_key = ''.join(secrets.choice(alphabet) for _ in range(256))
 clients = {}
 
 @app.route("/handshake")
@@ -34,7 +37,7 @@ def index():
     elif stage == "2":
         dh = clients[data['id']]
         key = dh.generate_full_key(data['pk1'])
-        #print(key)
+        # print(key)
         return "", 200
 
 @app.route("/get-message", methods=['POST'])
@@ -43,26 +46,53 @@ def get_message():
     dh = clients[data['id']]
     message = data['message'].encode()
     print(f"{data['id']}: {dh.decrypt_message(message)}")
+    # currently server not saving messages, will be added soon
     return "", 200
-
 
 class Server:
     def __init__(self):
         # starting tor
-        threading.Thread(target=subprocess.Popen, args=("../tor/tor/tor.exe -f ../tor/tor/torrc",),
-                        kwargs={'shell': False, 'stdout': subprocess.DEVNULL}).start()
+        self.messages = None
+        self.database = None
+        threading.Thread(target=self.start_tor).start()
+
+    def start_tor(self):
+        subprocess.Popen(["../tor/tor/tor.exe", "-f", "../tor/tor/torrc"],
+                         shell=False, stdout=subprocess.DEVNULL)
 
         # getting control over tor
-        controller = Controller.from_port(address="127.0.0.1", port=9051)
-        controller.authenticate(password="12345")  # don't ask about the password.
+        self.controller = Controller.from_port(address="127.0.0.1", port=9051)
+        self.controller.authenticate(password="12345")  # don't ask about the password.
 
-        # getting random open port
-        s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-        s.bind(("", 0))
-        s.listen(1)
-        self.port = s.getsockname()[1]
-        s.close()
-        a = controller.create_ephemeral_hidden_service(ports={80: f"127.0.0.1:{self.port}"}, await_publication=True)
-        self.service_host = list(a)[0].split("=")[1]
+    def start(self, key, name=None):
+        aes = AESCipher(key)
+        self.database = Storage(database_path, key)
+        self.messages = Storage(messages_path, key)
+
+        with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
+            s.bind(("", 0))
+            s.listen(1)
+            self.port = s.getsockname()[1]
+
+        if not os.path.exists(database_path):
+            a = self.controller.create_ephemeral_hidden_service(ports={80: f"127.0.0.1:{self.port}"},
+                                                               await_publication=True)
+            data = {
+                "service": f"{a.private_key_type}:{a.private_key}",
+                "name": name,
+                "address": a.service_id,
+                "contacts": {}
+            }
+            self.database.write(data)
+        else:
+            data = self.database.read()
+            print(data)
+            key_type, key_content = data['service'].split(":", 1)
+
+            a = self.controller.create_ephemeral_hidden_service(ports={80: f"127.0.0.1:{self.port}"},
+                                                               await_publication=True, key_type=key_type,
+                                                               key_content=key_content)
+
+        self.service_host = a.service_id
         print(self.service_host)
         threading.Thread(target=app.run, kwargs={'port': self.port}).start()
